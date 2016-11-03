82963d55fe3b6e1df2835d0cda95a29d42a622e9
Lubu909 <aizen1994@gmail.com>
2016-11-03 20:08:00 +0100

    Projekt bazy; skrypt do HSQLDB

--- src/test/ConnectionTest.java
+++ src/test/ConnectionTest.java
@@ -5,15 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import manager.CharacterManager;
+import manager.AccountManager;
+import manager.CharacterListManager;
 
 public class ConnectionTest {
 	
 	CharacterManager characterManager = new CharacterManager();
+	AccountManager accountManager = new AccountManager();
+	CharacterListManager characterListManager = new CharacterListManager();
 	
 	@Test
 	public void checkConnection(){
 		assertNotNull(characterManager.getConnection());
+		assertNotNull(accountManager.getConnection());
+		assertNotNull(characterListManager.getConnection());
 	}
+	
+	/*@Test
+	public void checkList(){
+		addAccount();
+		addCharacter();
+		addCharacter();
+		viewCharacterList();
+	}*/
 
 	/*@Test
 	public void test() {
